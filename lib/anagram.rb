9faa39c4495cd42400712045b1002510d78914df
Mondo Gao <mondogao@gmail.com>
2017-01-29 10:30:32 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :ori_word
+
+  def initialize(ori_word)
+    self.ori_word = ori_word
+  end
+
+  def match(words)
+    words.select do |word|
+      word.split(//).sort == self.ori_word.split(//).sort
+    end
+  end
+end
