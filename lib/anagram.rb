1e1ef96492991ddad3b9235732a5d03f01e666e7
Edson Mendieta <emendietaj@gmail.com>
2017-01-30 08:01:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(wordlist)
+        wordSort = @word.split("").sort
+        wordlist.find_all do |word|
+            wordSort == word.split("").sort
+        end
+    end
+end
