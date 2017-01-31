8f2c03a578cd7c54bda81ed87a857040f1220fdb
7s Dream <7seconddream@gmail.com>
2017-01-31 14:53:43 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :what
+
+  def initialize(what)
+    @what = what
+  end
+
+  def match(something)
+    sorted = what.chars.sort
+    something.select do |word|
+      word.chars.sort == sorted
+    end
+  end
+
+end
