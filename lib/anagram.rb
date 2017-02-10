1d3c1382b7a3b838aeb80b12462609b7e6bc3096
Hiram Cruz <hiram0101@gmail.com>
2017-02-10 17:18:19 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+
+  def initialize(string)
+    @string = string
+  end
+
+  def match(patterns)
+    letters = @string.split("").sort.join("")
+    patterns.select do |pattern|
+      pattern if pattern.split("").sort.join("").eql?(letters)
+    end
+  end
+end
