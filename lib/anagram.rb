340b68018a59d960b4662eb812db19498416e908
Adrian Torres <beowolfmon_1@hotmail.com>
2017-03-18 18:57:14 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(patterns)
+    matches = []
+    patterns.each do |pattern|
+      if self.word.length == pattern.length && self.word.split("").sort == pattern.split("").sort
+        matches << pattern
+      end
+    end
+    matches
+  end      
+end
