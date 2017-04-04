b7444354f0a3e92d8715a77e371d39d0e671c80a
Federico Gobbo <federico.gobbo.2@studenti.unipd.it>
2017-04-04 15:43:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.select do |this_word|
+      word.split('').sort == this_word.split('').sort
+    end
+  end
+
+end
