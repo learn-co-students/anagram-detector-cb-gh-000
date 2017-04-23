5b5764d5f1050dcd97c22dfdcffc1bee00d242ea
ahmed aboukoura <ahmedaboukoura1993@gmail.com>
2017-04-23 19:47:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
-# Your code goes here!
+class Anagram
+attr_accessor :word
+def initialize(word)
+ @word = word
+end
+def match(array)
+  word = @word.split(//).sort
+  array.find_all{|w|word == w.split('').sort }
+end
+end
