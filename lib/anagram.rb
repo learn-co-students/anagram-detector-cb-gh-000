1be8bee1b1746dda1dcc160ef2c903083f996190
Tuguldur Baigalmaa <philoniare@gmail.com>
2017-06-01 16:58:59 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+  def anagram?(word1, word2)
+    count1 = Hash.new(0)
+    word1.split("").each{|char| count1[char] += 1}
+    is_anagram = true
+    word2.split("").each{|char|
+      if(count1.key?(char) && count1[char] > 0)
+        count1[char] -= 1
+      else
+        is_anagram = false
+      end
+    }
+    is_anagram
+  end
+  def match(array)
+    array.select{|word| self.anagram?(@word, word)}
+  end
+end
