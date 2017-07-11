daa597afd958c01bd2f8b589775e970bdc7529a7
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2017-07-11 23:41:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    matches = []
+
+    words.each do |word|
+      is_anagram = true
+
+      word.each_char do |c|
+        if @word.count(c) < word.count(c)
+          is_anagram = false
+        end
+      end
+
+      matches << word if is_anagram
+    end
+
+    matches
+  end
+end
