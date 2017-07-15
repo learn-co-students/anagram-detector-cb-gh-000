e671055c3841f65e4b803c0fe9f36801aed2bc6e
Jade Anderson <enighter@gmail.com>
2017-07-15 17:03:35 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word.upcase.chars.sort.join
+  end
+
+  def match(array)
+    matches = []
+    array.each do |word|
+      matches << word if @word == word.upcase.chars.sort.join
+    end
+    matches
+  end
+
+end
