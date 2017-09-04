68f428bc7dd856c08b3adff986b959f0e9e18341
Hossam Hosny <hossam.hosni.95@gmail.com>
2017-09-04 20:26:31 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram
+	def initialize(word)
+		@word = word
+	end
+
+	def match(word_array)
+		word_array.find_all {|word| word.split("").sort.join == @word.split("").sort.join }
+	end
+end
