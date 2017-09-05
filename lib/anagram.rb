1956e1b8dcee2139cf620ff825f0a411462d8d95
Melissa Sheehan <mxsheehan7@gmail.com>
2017-09-05 00:11:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word #created a getter and setter method (def word=(word) and def word )
+  #different than the instance variables, @word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    matches = []
+    array.each do |word|
+      if word.split("").sort == @word.split("").sort
+          matches << word
+      end
+    end
+    matches
+  end
+
+end
+
+
+# diaper = Anagram.new('diaper')
+# diaper.match(%w(hello world zombies pants dipper))
+
+# a = Anagram.new("cat")
+# a.word
