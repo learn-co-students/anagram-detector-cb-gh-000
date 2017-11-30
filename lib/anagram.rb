7ec32344f2ce4b1a6106877853798f78b1636e79
Kid Coder <toddacodda@gmail.com>
2017-11-30 16:18:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    self.word = word
+  end
+
+  def match(words)
+    words.select { |e| e.chars.sort == self.word.chars.sort }
+  end
+end
