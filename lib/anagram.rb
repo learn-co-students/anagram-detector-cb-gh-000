7e47a206a20321a09533fcf8274f55ade4f4c494
Gene Gi <junzheji@gmail.com>
2017-12-21 06:22:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    word_char = @word.split("").sort
+    result = []
+    words.each {|word|
+      result << word if word.split("").sort == word_char
+    }
+    result
+  end
+end
