3ae4cc2e15c5bea3850761d96ea6775eba903ba0
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-18 12:12:41 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words_array)
+    words_array.collect { |word| 
+    word if @word.split("").sort == word.split("").sort 
+    }.compact
+  end
+  
+end
