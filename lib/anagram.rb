a85fc696b4302ff8581ce197c1271f64e88bc5fb
Krishan Jethwa <krishan.jethwa@student.manchester.ac.uk>
2018-06-23 22:39:27 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array) #array is the list of words which could be an anagrams
+    matches = []
+    array.each do |anagram| #gets each word in the list
+      split_word = @word.split("")
+      split_anagram = anagram.split("")
+      counter = 0 #checking each word
+      split_anagram.each do |letter|
+        puts letter
+        if split_word.include?(letter)
+          split_word.delete_at(split_word.index(letter))
+          counter += 1
+          puts "adds one for #{anagram}"
+        end
+      end
+      if counter == anagram.length
+        matches << anagram
+      end
+    end
+    return matches
+  end
+
+end
+
+songa = Anagram.new("allergy")
+songa.match(["gallery","regally","largely"])
