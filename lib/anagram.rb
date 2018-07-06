a9dc15bdac4eef74472de6c46245573afaef8ae0
Pranav Sharma <ps20maths@gmail.com>
2018-07-06 17:08:43 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagrams
+  def initialize(anagram)
+    @anagrams = anagram
+
+  end
+
+  def match(words)
+    anagram = []
+   words.each do |k|
+      if @anagrams.chars.all? {|char| k.include?(char) if @anagrams.size == k.size}
+        anagram << k
+      end
+   end
+   anagram
+ end
+end
