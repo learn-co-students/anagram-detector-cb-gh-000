f6f380cc1c8365441b14e6612cea44d0e68fc5cd
Ertugrul Dal <ertugrul.dal@ogr.sakarya.edu.tr>
2018-07-11 15:55:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(array)
+    array.collect do |word|
+      word_array = word.split("")
+      anagram_array = self.anagram.split("")
+     word if word_array.sort == anagram_array.sort
+   end.flatten.compact
+  end
+end
