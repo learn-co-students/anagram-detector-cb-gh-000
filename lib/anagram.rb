816543074f443db06edd1ca93393969ee6df09db
Quinn Dizon <qdizon@gmail.com>
2018-07-21 22:29:08 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+class Anagram
+
+  def initialize(test)
+    @test = test
+  end
+
+  def match(arr)
+    sorted = arr.collect do |word|
+      word.chars.sort.join
+    end
+
+    matched = []
+    arr.each_index do |index|
+      if @test.chars.sort.join == sorted[index]
+        matched << arr[index]
+      end
+    end
+    matched
+  end
+
+end
+
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets banana))
