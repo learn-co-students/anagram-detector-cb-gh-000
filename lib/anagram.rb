727d0028f30c437d9106e7017d6672f5c078081b
Koy Saeteurn <kosatun@aol.com>
2018-08-01 03:21:01 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram 
+  attr_accessor :name 
+  
+  
+  def initialize(word)
+    @name = word 
+  end   
+  
+  def match(array)
+    array.select do |list_word|
+       list_word.split("").sort == @name.split("").sort
+      end 
+    end 
+end   
