189fd943924731da8ba0898b5d491df4fcb18359
Irvin R <reyes.yerp@gmail.com>
2019-01-20 03:51:37 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    w1 = @word.downcase.split("").sort.join
+    array.select { |w2| w1 == w2.downcase.split("").sort.join }
+  end
+
+end
