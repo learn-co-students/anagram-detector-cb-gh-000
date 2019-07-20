5b3976cc5abd73a7c01a1fea420b9fed0a8a7b01
Michal Franczel <michal.franczel@gmail.com>
2019-07-20 20:34:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    ans = []
+    tempWord = @word
+
+    anagrams.each do |ana|
+      if(ana.length == tempWord.length)
+        count = 0
+        ana.split('').each do |char|
+          if(!tempWord.include?(char))
+            break
+          end
+          count += 1
+        end
+        if count == ana.length
+          ans << ana
+        end
+      end
+    end
+
+    return ans
+  end
+end
