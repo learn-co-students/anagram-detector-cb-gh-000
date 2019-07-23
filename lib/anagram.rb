7fbefac2796582f8a976c07421c86d4ffaf62f5a
SAFA YILDIRIM <safayildirim@anadolu.edu.tr>
2019-07-23 16:51:14 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+  attr_accessor:word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    returned_array = []
+    sum = 0
+    word.each_char { |chr| sum += chr.ord }
+    anagrams.each do |item|
+      sum2 = 0
+      item.each_char { |chr| sum2 += chr.ord }
+      if sum == sum2
+        returned_array << item
+      end
+    end
+    returned_array
+  end
+end
