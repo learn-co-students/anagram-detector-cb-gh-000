7d02d8c2369016600139a8dbcb10bb006adb59f7
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-11 13:16:08 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    match =[]
+    comp = self.word.split("").sort
+    array.each do |word|
+      if  comp == word.split("").sort
+        match << word
+      end
+    end
+    match
+  end
+end
