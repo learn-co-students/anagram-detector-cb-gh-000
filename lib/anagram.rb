15dcda29d03a261f69e6f7a532f60053d3776fb5
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-13 16:27:46 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize (word)
+    @word=word
+  end
+
+  def match(sentenece)
+    arr=Array.new
+    arr=@word.split("")
+    arr=arr.sort
+    arr1=Array.new
+    arr2=Array.new
+    sentenece.each do |w|
+      arr1=[]
+      arr1=w.split("")
+      if arr1.sort==arr
+        arr2<<w
+      end
+
+    end
+    return arr2
+  end
+
+end
